ebe50f757510ece7a1b6b782a7f7347b4a5b4fe1
La Fountain <github email address>
2019-01-27 21:15:02 -0500

    Add testing rspec

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    username { "MyString" }
+    email { "MyString" }
+    auth_token { "MyString" }
+    password_digest { "MyString" }
+  end
+end
